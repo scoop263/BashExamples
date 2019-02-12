f46d287bc00ed40b30b65a22c1511175bcdfb4ea
scoop263 <sdeditius1995@gmail.com>
2019-02-12 15:55:19 -0800

    Killing Terminal Processes

---
+++ KillTerminalsSmileyFace.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# KillTerminalSmileyFace.sh
+# This script will kill any and all
+# terminals
+set -x
+#-------- Functions ----------
+terminalName() {
+	if [[ -z $1 ]]; then
+		exit 0
+	elif [[ $1 = "not a tty" ]]; then
+		exit 0
+	fi
+}
+
+#------ Variables ---------
+ZETERMINAL=$(tty)
+PROCESSCONTENTS=$(mktemp ~/pscommand.XXXXXXXXX)
+PARENTPROCESSID=$(mktemp ~/ppid.XXXXXXXXX)
+#------- Main ----------
+terminalName $ZETERMINAL
+ps -ef | egrep "su|sshd:" > $PROCESSCONTENTS
+sed -i "/@/d; /su/d; /ps -ef/d; /grep/d; /"$0"/d" $PROCESSCONTENTS
+awk '{print $2}' $PROCESSCONTENTS > $PARENTPROCESSID
+while read i; do
+	pkill -KILL -P $i
+done < $PARENTPROCESSID
